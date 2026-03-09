c656e3a55d9a9a45ba5befa5f1a84ebcb67c10c3
lachkar-abdel <79232484+lachkar-abdel@users.noreply.github.com>
2026-03-09 00:57:13 +0000

    Update app.e97e5b83.js

--- js/app.e97e5b83.js
+++ js/app.e97e5b83.js
@@ -1,2 +1,168 @@
-(function(t){function e(e){for(var n,r,a=e[0],c=e[1],l=e[2],d=0,u=[];d<a.length;d++)r=a[d],Object.prototype.hasOwnProperty.call(o,r)&&o[r]&&u.push(o[r][0]),o[r]=0;for(n in c)Object.prototype.hasOwnProperty.call(c,n)&&(t[n]=c[n]);p&&p(e);while(u.length)u.shift()();return s.push.apply(s,l||[]),i()}function i(){for(var t,e=0;e<s.length;e++){for(var i=s[e],n=!0,a=1;a<i.length;a++){var c=i[a];0!==o[c]&&(n=!1)}n&&(s.splice(e--,1),t=r(r.s=i[0]))}return t}var n={},o={app:0},s=[];function r(e){if(n[e])return n[e].exports;var i=n[e]={i:e,l:!1,exports:{}};return t[e].call(i.exports,i,i.exports,r),i.l=!0,i.exports}r.m=t,r.c=n,r.d=function(t,e,i){r.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:i})},r.r=function(t){"undefined"!==typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},r.t=function(t,e){if(1&e&&(t=r(t)),8&e)return t;if(4&e&&"object"===typeof t&&t&&t.__esModule)return t;var i=Object.create(null);if(r.r(i),Object.defineProperty(i,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var n in t)r.d(i,n,function(e){return t[e]}.bind(null,n));return i},r.n=function(t){var e=t&&t.__esModule?function(){return t["default"]}:function(){return t};return r.d(e,"a",e),e},r.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},r.p="/";var a=window["webpackJsonp"]=window["webpackJsonp"]||[],c=a.push.bind(a);a.push=e,a=a.slice();for(var l=0;l<a.length;l++)e(a[l]);var p=c;s.push([0,"chunk-vendors"]),i()})({0:function(t,e,i){t.exports=i("56d7")},"034f":function(t,e,i){"use strict";i("85ec")},"0d45":function(t,e,i){},"1b05":function(t,e,i){},3004:function(t,e,i){"use strict";i("b70e")},"303d":function(t,e,i){"use strict";i("0d45")},"56d7":function(t,e,i){"use strict";i.r(e);i("e260"),i("e6cf"),i("cca6"),i("a79d");var n=i("2b0e"),o=function(){var t=this,e=t.$createElement,i=t._self._c||e;return i("div",{staticClass:"container",attrs:{id:"app"}},[i("HeaderRS"),i("BodyRS"),i("FooterRS")],1)},s=[],r=function(){var t=this,e=t.$createElement,i=t._self._c||e;return i("div",[i("div",{staticClass:"row",attrs:{id:"projet-vue-js"}},[i("div",{staticClass:"col-md-4",attrs:{id:"title"}},[i("img",{staticStyle:{"border-radius":"50%"},attrs:{src:"https://scontent.frba2-1.fna.fbcdn.net/v/t1.18169-9/13240577_1115964741800786_2228495543524362209_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=h4S1rux4AdQAX-Icmz5&tn=82G5LEieNuCqAYUZ&_nc_ht=scontent.frba2-1.fna&oh=00_AT9N1hggduVDVDfFMpZkItTuEVyB3bXcfRHP6BLMCODTlQ&oe=62146627",height:"238",width:"250"}}),i("h1",{staticClass:"text-monospace"},[t._v(t._s(t.name.first+" "+t.name.last))]),i("p",{staticClass:"text-muted",attrs:{id:"position"}},[i("span",[t._v(t._s(t.name.position))])])]),i("div",{staticClass:"col-md-8"},[i("div",{staticClass:"item-contact"},t._l(t.contact,(function(e,n){return i("div",{key:n,staticClass:"text-muted contact",class:e.type},[e.link?[i("a",{attrs:{href:e.link}},[t._v(t._s(e.value))])]:[t._v(" "+t._s(e.value)+" ")]],2)})),0)])]),i("div",{staticClass:"row justify-content-center"},[i("div",{staticClass:"col-md-6 col-centered"},[i("div",{staticClass:"text-muted",attrs:{id:"about"}},[t._v(" "+t._s(t.name.about)+" ")])])])])},a=[],c={name:"HeaderRS",data:function(){return{logo:"./assets/avatar.jpg",name:{first:"Abdelghafor",middle:"",last:"Lachkar",about:"I am a full stack developer, I work projects concerning mobile app development and web development. For the moment, I am working as a freelancer. I am always ready to learn new technologies and start new projects. For any other information, feel free to contact me.",position:"FULL STACK DEVELOPER"},contact:[{type:"adress",value:"Sala al-Jadida, Rabat , Morocco"},{type:"mail",value:"Lachkar.dev@gmail.com"},{type:"github",value:"Github.com/lachkar-abdel",link:"http://github.com/lachkar-abdel"}]}}},l=c,p=(i("3004"),i("2877")),d=Object(p["a"])(l,r,a,!1,null,null,null),u=d.exports,m=function(){var t=this,e=t.$createElement,i=t._self._c||e;return i("div",{staticClass:"row"},[i("div",{staticClass:"col-md-6"},t._l(t.sections.filter((function(t){return!t.right})),(function(t,e){return i("SectionRS",{key:"section-left-"+e,attrs:{section:t}})})),1),i("div",{staticClass:"col-md-6"},t._l(t.sections.filter((function(t){return t.right})),(function(t,e){return i("SectionRS",{key:"section-right-"+e,attrs:{section:t}})})),1)])},f=[],h=function(){var t=this,e=t.$createElement,i=t._self._c||e;return i("section",[i("h2",{staticClass:"section-title"},[t._v(t._s(t.section.title))]),i("div",{staticClass:"section-head"}),i("div",{staticClass:"sections"},t._l(t.section.items,(function(e,n){return i("div",{key:n,staticClass:"section"},[i("h3",{staticClass:"subtitle"},[e.timeperiod?[t._v(t._s(e.timeperiod)+" - ")]:t._e(),t._v(" "+t._s(e.title)+" ")],2),e.descriptions?t._l(e.descriptions,(function(e,n){return i("div",{key:n,staticClass:"description"},[i("ul",[i("li",{staticClass:"text-muted",domProps:{innerHTML:t._s("- "+e)}})])])})):t._e()],2)})),0)])},v=[],g={name:"SectionRS",props:{section:Object}},b=g,S=(i("303d"),Object(p["a"])(b,h,v,!1,null,null,null)),_=S.exports,C={name:"BodyRS",components:{SectionRS:_},data:function(){return{sections:[{right:!1,title:"EXPERIENCE",items:[{timeperiod:"09/2021 → CURRENT",title:"DataFlex",descriptions:["Conception and Development of Mobile Application DawiDari.","Creation of a web API with <strong>ASP.NET Core</strong> MVC.","Development a Mobile App with <strong>Flutter</strong>."]},{timeperiod:"2016 → 2019",title:"Freelance Works",descriptions:["Creation of Mobile Applications with <strong>Java</strong>.","Creation of Web Applications with <strong>Angular</strong>."]},{timeperiod:2015,title:"Ministry Of Justice",descriptions:["Conception and Development of a Management Application.","Creation of <strong>ASP.NET</strong> Web Application Using MVC and Entity Framework.","Managment Project with Team Foundation Server (TFS)"]}]},{right:!0,title:"PROJECTS",items:[{timeperiod:"2021",title:"le17ocean.com",descriptions:["Creation of an Interactive Dashboards in <strong>Angular</strong>.",'Creation of the web interface with <strong>AMP</strong> technology, <u><a href="http://le17ocean.com/">17ocean</a></u>',"Creation of the web API with <strong>.net core</strong>.","Project conception and development using <strong>MEAN</strong> stack."]},{timeperiod:"2020",title:"Law Qcm",descriptions:['Creation of Mobile Applications with <strong>Java</strong>, <u><a href="https://play.google.com/store/apps/details?id=com.qcm.droit">Law Qcm</a></u>']},{timeperiod:"2020",title:"Multiple choice law questions",descriptions:['Creation of Mobile Applications with <strong>Java</strong>, <u><a href="https://play.google.com/store/apps/details?id=com.qcmdroit.arabe">Multiple choice</a></u>']},{timeperiod:"2019",title:"with the legal system QCM",descriptions:['Creation of Mobile Applications with <strong>Java</strong>, <u><a href="https://play.google.com/store/apps/details?id=com.qcmdroit.arabe">legal system</a></u>']}]},{right:!1,title:"STUDIES",items:[{timeperiod:2021,title:"Professional university license",descriptions:["Web and Mobile Applications Development at Ibn Tofail University."]},{timeperiod:2016,title:"Skills training",descriptions:["Web and Mobile Applications Development at SFIJ."]},{timeperiod:2014,title:"Specialized Technician Diploma",descriptions:["Specialized Technician Diploma in Software Development."]},{timeperiod:2012,title:"Scientific Baccalaureate",descriptions:["High-school Diploma in Biology"]}]},{right:!1,title:"CERTIFICATIONS",items:[{timeperiod:2018,title:"Udacity",descriptions:["Full Stack Developer"]},{timeperiod:2016,title:"Microsoft® Specialist",descriptions:["Microsoft® Specialist: Programming in HTML5 with JavaScript and CSS3."]}]},{right:!0,title:"SKILLS",items:[{title:"Teamwork and autonomy",descriptions:["Git, TFS"]},{title:"Technologies",descriptions:["C#, TypeScript, JavaScript"]},{title:"Web Technologies",descriptions:["Angular, VueJs, Node Js, AMP"]},{title:"Frameworks",descriptions:[" ASP.NET Core, Flutter"]},{title:"DataBase",descriptions:["SQL, NoSQL"]},{title:"DevOps and CI",descriptions:["Docker, Vagrant, Travis CI"]},{title:"Servers",descriptions:["Windows Server, AWS"]}]},{right:!0,title:"EXPERTISE",items:[{title:"Database Management"},{title:"Rest API Development"},{title:"Test Driven Development"}]}]}}},y=C,w=Object(p["a"])(y,m,f,!1,null,null,null),T=w.exports,A=function(){var t=this,e=t.$createElement,i=t._self._c||e;return i("div",{staticClass:"col-md-12"},[i("section",{staticClass:"interests"},[i("h2",{staticClass:"interests-title"},[t._v(t._s(t.interests.title))]),t._l(t.interests.items,(function(e,n){return[i("span",{key:n},[t._v(t._s(e.title)+" ")])]}))],2)])},M=[],k={name:"FooterRS",data:function(){return{interests:{title:"INTERESTS",template:"interests-section",items:[{title:"Traveling,"},{title:"football,"},{title:"Programming,"}]}}}},E=k,D=(i("68b3"),Object(p["a"])(E,A,M,!1,null,null,null)),P=D.exports,j={name:"app",components:{HeaderRS:u,BodyRS:T,FooterRS:P}},R=j,O=(i("034f"),Object(p["a"])(R,o,s,!1,null,null,null)),I=O.exports;i("1b05");n["a"].config.productionTip=!0,new n["a"]({render:function(t){return t(I)}}).$mount("#app")},"68b3":function(t,e,i){"use strict";i("98b5")},"85ec":function(t,e,i){},"98b5":function(t,e,i){},b70e:function(t,e,i){}});
-//# sourceMappingURL=app.e97e5b83.js.map
+const resumeData = {
+  name: 'Lachkar Abdelghafor',
+  role: 'Développeur Full Stack',
+  summary: "Développeur web et mobile avec une expérience solide en Symfony, Angular, Android et intégration d’APIs bancaires. Orienté performance, stabilité et développement de solutions robustes pour le web et les terminaux de paiement.",
+  contacts: [
+    { label: 'Téléphone', value: '06 74 43 76 65', href: 'tel:+212674437665' },
+    { label: 'Email', value: 'lachkar.dev@gmail.com', href: 'mailto:lachkar.dev@gmail.com' },
+    { label: 'Adresse', value: 'Riad alkawtar, Mohmadia' },
+    { label: 'Langues', value: 'Arabe · Français · Anglais' }
+  ],
+  experience: [
+    {
+      date: 'Depuis janvier 2023',
+      title: 'Développeur Android',
+      company: 'Youzcard Technology - Casablanca',
+      bullets: [
+        "Développement d'applications mobiles de paiement sur terminaux électroniques.",
+        "Intégration d’APIs bancaires et protocoles pour TPE.",
+        'Tests et optimisation des applications pour garantir performance et stabilité.'
+      ]
+    },
+    {
+      date: 'Septembre 2021 - Décembre 2022',
+      title: 'Développeur Informatique',
+      company: 'DataFlex - Rabat',
+      bullets: [
+        "Développement d'une API web avec ASP.NET Core MVC.",
+        "Implémentation front-end avec Angular pour des interfaces réactives.",
+        'Optimisation des bases de données et des requêtes SQL.'
+      ]
+    },
+    {
+      date: 'Février 2017 - Juin 2020',
+      title: 'Développeur Symfony & Angular',
+      company: 'CDI CONCEPT - Salé',
+      bullets: [
+        "Développement d'applications web : création et maintenance d'applications web complexes.",
+        'Utilisation de Symfony pour le backend et Angular pour le frontend.',
+        'Participation à la conception de solutions robustes et évolutives.'
+      ]
+    }
+  ],
+  education: [
+    {
+      date: '2021 - 2022',
+      title: 'Licence Professionnelle Universitaire',
+      school: "Développement d'application web et mobiles",
+      bullets: ['Faculté des Sciences Ibn Tofail - Kénitra']
+    },
+    {
+      date: '2012 - 2014',
+      title: 'Diplôme de Technicien Spécialisé',
+      school: 'Développement Informatique',
+      bullets: ['ISTA - Sala Al Jadida']
+    },
+    {
+      date: '2011 - 2012',
+      title: 'Baccalauréat',
+      school: 'Science de la Vie et de Terre',
+      bullets: ['Lycée Mohammed VI - Sala Al Jadida']
+    }
+  ],
+  skills: [
+    {
+      title: 'Langages de programmation',
+      items: ['PHP (Symfony)', 'JavaScript', 'HTML5', 'CSS3', 'TypeScript', 'Kotlin', 'Java']
+    },
+    {
+      title: 'Frameworks & Technologies',
+      items: ['Symfony', 'Angular', 'API RESTful', 'Doctrine', 'Twig', 'Bootstrap']
+    },
+    {
+      title: 'Outils de développement',
+      items: ['Git', 'Composer', 'NPM', 'Webpack']
+    },
+    {
+      title: 'Bases de données',
+      items: ['MySQL', 'PostgreSQL', 'SQL Server']
+    }
+  ],
+  interests: ['Lecture', 'Football']
+};
+
+function renderTimeline(items) {
+  return `
+    <div class="timeline">
+      ${items.map(item => `
+        <div class="timeline-item">
+          <div class="meta">${item.date}</div>
+          <h3 class="item-title">${item.title}</h3>
+          <div class="item-subtitle">${item.company || item.school}</div>
+          <ul>
+            ${(item.bullets || []).map(b => `<li>${b}</li>`).join('')}
+          </ul>
+        </div>
+      `).join('')}
+    </div>
+  `;
+}
+
+function renderSkills(groups) {
+  return groups.map(group => `
+    <div class="skill-group">
+      <h3>${group.title}</h3>
+      <div class="chips">
+        ${group.items.map(item => `<span class="chip">${item}</span>`).join('')}
+      </div>
+    </div>
+  `).join('');
+}
+
+function renderContacts(contacts) {
+  return contacts.map(contact => `
+    <div class="contact-card">
+      <span class="contact-label">${contact.label}</span>
+      ${contact.href
+        ? `<a class="contact-value" href="${contact.href}">${contact.value}</a>`
+        : `<span class="contact-value">${contact.value}</span>`}
+    </div>
+  `).join('');
+}
+
+const app = document.getElementById('app');
+app.innerHTML = `
+  <div class="container">
+    <section class="hero">
+      <div class="hero-left">
+        <div class="badge">Curriculum Vitae</div>
+        <h1>${resumeData.name}</h1>
+        <div class="role">${resumeData.role}</div>
+        <p class="summary">${resumeData.summary}</p>
+      </div>
+      <div class="hero-right">
+        ${renderContacts(resumeData.contacts)}
+      </div>
+    </section>
+
+    <section class="main-grid">
+      <div>
+        <article class="card">
+          <h2 class="section-title">Expérience professionnelle</h2>
+          ${renderTimeline(resumeData.experience)}
+        </article>
+
+        <article class="card" style="margin-top:24px;">
+          <h2 class="section-title">Formation</h2>
+          ${renderTimeline(resumeData.education)}
+        </article>
+      </div>
+
+      <div>
+        <article class="card">
+          <h2 class="section-title">Compétences</h2>
+          ${renderSkills(resumeData.skills)}
+        </article>
+
+        <article class="card" style="margin-top:24px;">
+          <h2 class="section-title">Centres d’intérêt</h2>
+          <div class="chips">
+            ${resumeData.interests.map(item => `<span class="chip">${item}</span>`).join('')}
+          </div>
+        </article>
+      </div>
+    </section>
+
+    <p class="footer-note">Version web mise à jour à partir du CV PDF.</p>
+  </div>
+`;
